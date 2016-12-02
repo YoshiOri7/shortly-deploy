5b3b37f22b705194b485a5247fdd71b59e0d52f8
Hack Reactor Students <accounts@hackreactor.com>
2016-12-01 17:00:24 -0800

    Refactoring using moongoose/mongoDB, passing a fews tests.

--- app/config.js
+++ app/config.js
@@ -1,40 +1,75 @@
-var path = require('path');
-var knex = require('knex')({
-  client: 'sqlite3',
-  connection: {
-    filename: path.join(__dirname, '../db/shortly.sqlite')
-  },
-  useNullAsDefault: true
-});
-var db = require('bookshelf')(knex);
+// var path = require('path');
+// var knex = require('knex')({
+//   client: 'sqlite3',
+//   connection: {
+//     filename: path.join(__dirname, '../db/shortly.sqlite')
+//   },
+//   useNullAsDefault: true
+// });
+// var db = require('bookshelf')(knex);
 
-db.knex.schema.hasTable('urls').then(function(exists) {
-  if (!exists) {
-    db.knex.schema.createTable('urls', function (link) {
-      link.increments('id').primary();
-      link.string('url', 255);
-      link.string('baseUrl', 255);
-      link.string('code', 100);
-      link.string('title', 255);
-      link.integer('visits');
-      link.timestamps();
-    }).then(function (table) {
-      console.log('Created Table', table);
-    });
-  }
+// db.knex.schema.hasTable('urls').then(function(exists) {
+//   if (!exists) {
+//     db.knex.schema.createTable('urls', function (link) {
+//       link.increments('id').primary();
+//       link.string('url', 255);
+//       link.string('baseUrl', 255);
+//       link.string('code', 100);
+//       link.string('title', 255);
+//       link.integer('visits');
+//       link.timestamps();
+//     }).then(function (table) {
+//       console.log('Created Table', table);
+//     });
+//   }
+// });
+
+// db.knex.schema.hasTable('users').then(function(exists) {
+//   if (!exists) {
+//     db.knex.schema.createTable('users', function (user) {
+//       user.increments('id').primary();
+//       user.string('username', 100).unique();
+//       user.string('password', 100);
+//       user.timestamps();
+//     }).then(function (table) {
+//       console.log('Created Table', table);
+//     });
+//   }
+// });
+
+// module.exports = db;
+// grab the things we need
+
+// ========================================================================
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var dbSchema = {};
+
+// create a schema
+var userSchema = new Schema({
+  username: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  created_at: Date,
+  updated_at: Date
 });
 
-db.knex.schema.hasTable('users').then(function(exists) {
-  if (!exists) {
-    db.knex.schema.createTable('users', function (user) {
-      user.increments('id').primary();
-      user.string('username', 100).unique();
-      user.string('password', 100);
-      user.timestamps();
-    }).then(function (table) {
-      console.log('Created Table', table);
-    });
-  }
+// the schema is useless so far
+// we need to create a model using it
+dbSchema.userSchema = userSchema;
+
+
+var urlSchema = new Schema({
+  url: String,
+  baseUrl: String,
+  code: String,
+  title: String,
+  visits: {type: Number, default: 0},
+  created_at: Date,
+  updated_at: Date
 });
 
-module.exports = db;
+dbSchema.urlSchema = urlSchema;
+
+// make this available to our users in our Node applications
+module.exports = dbSchema;
